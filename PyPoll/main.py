7af59142c025632010b443dcfd204f6cd520bfe9
patrickm96 <135382512+patrickm96@users.noreply.github.com>
2023-07-13 22:06:26 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,40 +1,48 @@
+#Import modules
 import os
 import csv
 import numpy as np
-import array as arr
 
-csvpath = os.path.join('.', 'Resources','election_data.csv')
+#Path to .csv file
+csvpath = os.path.join('.', 'PyPoll', 'Resources','election_data.csv')
 
+#List for Ballot IDs
 ballot_id = []
+#List for County Names
 county_name = []
+#List for Candidate Names
 candidate_name = []
+#Unique list of candidate names
 unique_candidate_names = []
+#List for vote counts for each candidate
 vote_counts = []
 
-
+#Open .csv file
 with open(csvpath,"r") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
-    #print(csvreader)
-
+    #Skip header in .csv file
     csv_header = next(csvreader)
-
+    #Populate lists
     for row in csvreader:
         ballot_id.append(int(row[0]))
         county_name.append(row[1])
         candidate_name.append(row[2])
+        #Get count of votes
         total_vote_count = len(ballot_id)
-
+#Create list of unique candidate names
 unique_candidate_names = np.append(unique_candidate_names, np.unique(candidate_name))
+
+#Create identifiers for each candidate
 candidate_1_name = unique_candidate_names[0]
 candidate_2_name = unique_candidate_names[1]
 candidate_3_name = unique_candidate_names[2]
 
+#Vote count for each candidate starts at 0
 candidate_1_vote_count = 0
 candidate_2_vote_count = 0
 candidate_3_vote_count = 0
 
-#print(candidate_1_vote_count + 1)
-
+#Count number of times each candidate's name is listed
 for i in candidate_name:
     if i == candidate_1_name:
         candidate_1_vote_count = candidate_1_vote_count + 1
@@ -43,37 +51,23 @@ for i in candidate_name:
     elif i == candidate_3_name:
         candidate_3_vote_count = candidate_3_vote_count + 1
 
-#candidate_1_vote_count = candidate_name.count(candidate_1_name)
-
+#Populate vote_counts list with number of votes for each candidate in order of unique_candidate_names list
 vote_counts.append(candidate_1_vote_count)
 vote_counts.append(candidate_2_vote_count)
 vote_counts.append(candidate_3_vote_count)
 
-#print(vote_counts)
-#max_vote_counts = str(np.where(vote_counts == max(vote_counts))[0][0])
-#print(max_vote_counts)
-#max_vote_counts_integer = int(max_vote_counts)
-#max_votes_winner = unique_candidate_names[max_vote_counts_integer]
-
+#Get index/position of max number in vote_counts list
 max_vote_count_index = vote_counts.index(max(vote_counts))
 
-#print(max_vote_count_index)
+#Return name from unique_candidate_names list that is in the position of max number in vote_counts list
 candidate_winner = unique_candidate_names[max_vote_count_index]
 
-#print(candidate_1_name)
-#print(candidate_1_vote_count)
-#print(candidate_2_vote_count)
-#print(candidate_3_vote_count)
-
+#Calculate vote percentage for each candidate
 candidate_1_vote_percent = "{:.3%}".format(candidate_1_vote_count / total_vote_count)
 candidate_2_vote_percent = "{:.3%}".format(candidate_2_vote_count / total_vote_count)
 candidate_3_vote_percent = "{:.3%}".format(candidate_3_vote_count / total_vote_count)
 
-#print(candidate_1_vote_percent)
-
-#print(total_votes)
-#print(unique_candidate_names)
-
+#Print results
 print("Election Results")
 print("-----------------------------")
 print(f'Total Votes: {total_vote_count}')
